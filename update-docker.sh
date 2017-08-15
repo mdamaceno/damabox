e0c5dbff1495c990f547dffea0bbc1debcaecce5
mdamaceno <maadamaceno@gmail.com>
2017-08-15 13:57:04 -0300

    Script para atualizar imagens

---
+++ update-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull maadamaceno/damabox_php5.5:latest
+docker pull maadamaceno/damabox_php5.6:latest
+docker pull maadamaceno/damabox_php7.0:latest
+docker pull maadamaceno/damabox_php7.1:latest
+docker pull maadamaceno/damabox_nginx:latest
+docker pull maadamaceno/damabox_firebird:latest
